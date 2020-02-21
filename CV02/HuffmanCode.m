2ec9b38d07e022488644a42d3f59e6913655f3a4
vymaztom <vyma.96@gmail.com>
2020-02-21 08:34:57 +0100

    CV02 added

---
+++ CV02/HuffmanCode.m
@@ -0,0 +1,8 @@
+function ret=huffmanc(InputMatrix)
+  % input value [1 0.1 ; 2 0.2 ; 3 0.3 ; 4 0.15 ; 5 0.5]
+  sortMatrix = sortrows(InputMatrix,2)
+  for one=1:size(sortMatrix)(1)-1
+    sortMatrix =   
+  endfor
+  
+endfunction
