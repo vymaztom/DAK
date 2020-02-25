4988ebfe78dcb46fc6aeccb49d9ed9f22e7a396b
vymaztom <vyma.96@gmail.com>
2020-02-25 13:30:00 +0100

    create odd PariteBit function

--- CV02/pariteBitOdd.m
+++ CV02/pariteBitOdd.m
@@ -1,11 +1,11 @@
-function ret = pariteBit(input)
+function ret = pariteBitOdd(input)
   ret = [];
   sum = 0;
   pos = 0;
   for one=1:size(input)(1,2)
     sum = sum + input(1,one);
     if mod(one,8) == 0;
-      ret = [ret input(pos*8+1:(pos+1)*8) mod(sum+1,2)];
+      ret = [ret input(pos*8+1:(pos+1)*8) mod(sum,2)];
       sum = 0;
       pos = pos + 1;
     endif
